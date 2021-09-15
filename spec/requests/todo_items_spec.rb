ce6ee02b36e4d978a610861dc3d05545e2601a9a
solidlikeplato <ethanwhaley@gmail.com>
2021-09-15 16:18:41 -0500

    linked show page to update title and description

--- spec/requests/todo_items_spec.rb
+++ spec/requests/todo_items_spec.rb
@@ -38,6 +38,13 @@ RSpec.describe 'TodoItems', type: :request do
     expect(response.body).to match /East of Eden/
   end
 
+  it 'shows change description form' do
+    item1 = FactoryBot.create(:todo_item, title: "Of Mice and Men", description: "George and Lenny", is_done: true)    
+    get "/todo_items/#{item1.id}/change_description"
+
+    expect(response).to render_template(:change_description)
+  end
+
   it 'changes the description on patch request' do
     item1 = FactoryBot.create(:todo_item, title: "Of Mice and Men", description: "George and Lenny", is_done: true)    
     patch "/todo_items/#{item1.id}", params: {description: "the Trasks and the Hamiltons"}
