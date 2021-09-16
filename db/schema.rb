3f110884104a1b5de14b0922e492f3e89b093f13
solidlikeplato <ethanwhaley@gmail.com>
2021-09-16 11:32:41 -0500

    modified tests to include author todo-item relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_150950) do
+ActiveRecord::Schema.define(version: 2021_09_16_154741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,14 @@ ActiveRecord::Schema.define(version: 2021_09_16_150950) do
   end
 
   create_table "todo_items", force: :cascade do |t|
-    t.string "author"
     t.string "title"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_done", default: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_todo_items_on_author_id"
   end
 
+  add_foreign_key "todo_items", "authors"
 end
--- spec/factories/todo_item.rb
+++ spec/factories/todo_item.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :todo_item, class: TodoItem do
-    author { 'Isaac Asimov' }
+    author { association :author }
     title { 'Write "I, Robot"'}
     description { 'How do we program robots?' }
     is_done { false }
