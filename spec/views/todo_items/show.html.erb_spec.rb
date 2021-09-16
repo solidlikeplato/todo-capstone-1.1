8b2a994d81eab2fe07b90e52aeddf80d6b817b43
solidlikeplato <ethanwhaley@gmail.com>
2021-09-16 10:04:30 -0500

    corrected depricated test formatting

--- spec/views/todo_items/show.html.erb_spec.rb
+++ spec/views/todo_items/show.html.erb_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "todo_items/show.html.erb" do
 
   it 'passes correct params' do
     item1 = FactoryBot.create(:todo_item, title: "Of Mice and Men", description: "George and Lenny")
-    controller.extra_params = { :id => item1.id }
+    controller.extra_params = { id: item1.id }
     
     expect(controller.request.fullpath).to eq todo_item_path(item1)
   end
@@ -12,9 +12,9 @@ RSpec.describe "todo_items/show.html.erb" do
   it 'renders correct params' do
     item1 = FactoryBot.create(:todo_item, title: "Of Mice and Men", description: "George and Lenny", is_done: false)
     assign(:todo_item, item1)
-    controller.extra_params = { :id => item1.id }
+    controller.extra_params = { id: item1.id }
     
-    render :template => 'todo_items/show.html.erb'
+    render(template: 'todo_items/show')
 
     expect(rendered).to match /Of Mice and Men/
     expect(rendered).to match /George and Lenny/
