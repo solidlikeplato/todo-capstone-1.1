9db549d4e356d8ee28af7ebd6fe735ea5dad271d
solidlikeplato <ethanwhaley@gmail.com>
2021-09-17 10:11:17 -0500

    can now add an item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,8 +1,11 @@
 class TodoItemsController < ApplicationController
-  before_action :set_author, only: [:create]
-  before_action :set_todo_item, except: [:create]
+  before_action :set_author, only: [:create, :new]
+  before_action :set_todo_item, except: [:create, :new]
 
   def show
+  end
+
+  def new
   end
 
   def create
@@ -11,7 +14,8 @@ class TodoItemsController < ApplicationController
   end
 
   def update
-    @todo_item.update!(todo_item_params)
+    @todo_item.update(todo_item_params)
+    
     redirect_to @todo_item
   end
 
@@ -23,13 +27,14 @@ class TodoItemsController < ApplicationController
   private
   def set_todo_item
     @todo_item = TodoItem.find(params[:id])
+    print @todo_item
   end
 
   def set_author
-    @author = Author.find(params[:id])
+    @author = Author.find(params[:author_id])
   end
 
   def todo_item_params
-    params.permit(:title, :description, :author, :is_done)
+    params[:todo_item].permit(:title, :description,:author, :is_done)
   end
 end
